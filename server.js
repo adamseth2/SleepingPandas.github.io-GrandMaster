1f98c03a22a5e9df5bb4958d74c0c6f694f02b9a
adamseth2 <chhoradam@gmail.com>
2021-06-07 00:52:12 -0700

    server completed for basic mutiplayer

--- server.js
+++ server.js
@@ -8,10 +8,29 @@ const server = http.createServer(app);
 const io = socketIo(server);
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'games')));
 
 //Run when client connects
 io.on('connection', socket => {
   console.log('New WS Connection...');
+  socket.on('clickBoard', number => {
+    console.log(number);
+  });
+  //When user joins
+  socket.broadcast.emit('console', 'A user has joined the chat');
+  //disconnects
+  socket.on('disconnect', () => {
+    io.emit('console', 'A user has left the chat');
+  });
+
+  //Listen for valid click board
+  socket.on('clickBoard', squareId => {
+    socket.broadcast.emit('clickBoardHandler', squareId);
+  });
+  //Restarts Game
+  socket.on('restart', player1Turn => {
+    socket.broadcast.emit('restartHanlder', player1Turn);
+  });
 });
 
 const PORT = 3000 ?? process.env.PORT;
