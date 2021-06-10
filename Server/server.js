d345c8299e710e416a1269769356645205c61f34
adamseth2 <chhoradam@gmail.com>
2021-06-09 20:57:17 -0700

    Added alphabet to makeId

--- Server/server.js
+++ Server/server.js
@@ -102,7 +102,7 @@ io.on('connection', socket => {
 //Make unique ID
 function makeId(length) {
   var result = '';
-  var characters = '0123456789';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
